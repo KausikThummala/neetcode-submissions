e17fb6aa76ea883cfab7d8624be7341a37fa3186
bigblok456 <kausik72005@gmail.com>
2026-07-05 00:15:06 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res=helper(x,abs(n));
+        return ((n>=0) ? res:(1/res));
+        
+    }
+    double helper(double x,int n){
+        if(x==0) return 0;
+        if(n==0) return 1;
+        
+        double res=helper(x,n/2);
+        res=res*res;
+        return ((n%2==1) ? x*res:res);
+    }
+};
